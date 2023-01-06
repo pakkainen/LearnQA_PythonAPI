a18393e98e3b9cc3aaf4ff3cf90eba6a3e2b7fca
Ivan Pustovalov <ipustovalov@swordfishsecurity.com>
2023-01-06 20:41:44 +0300

    check_redirect.py for Ex6 correction

--- check_redirect.py
+++ check_redirect.py
@@ -1,5 +1,7 @@
 import requests
 
-response = requests.get("https://playground.learnqa.ru/api/long_redirect")
+response = requests.get("https://playground.learnqa.ru/api/long_redirect", headers={
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:32.0) Gecko/20100101 Firefox/32.0'})
 print(f"Произошло {len(response.history)} редиректа")
 print("Итоговый URL:", response.url)
+print("Код ответа:", response.status_code)
