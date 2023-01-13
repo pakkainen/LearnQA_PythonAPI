bc375070ab3967258eba5943a251ce7c578c850f
Ivan Pustovalov <ipustovalov@swordfishsecurity.com>
2023-01-14 00:09:29 +0300

    test_phrase.py for Ex10 fix critical len variable

--- test_phrase.py
+++ test_phrase.py
@@ -1,5 +1,5 @@
 class TestPhrase:
     def test_check_phrase(self):
-        critical_len = 14
+        critical_len = 15
         phrase = input(f"Please, set a phrase shorter than {critical_len} characters: ")
         assert len(phrase) < critical_len, f"Your phrase is not shorter than {critical_len} characters"
