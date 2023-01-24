f20381036d13e99e6c22ce6504479ddd6cb153cd
Ivan Pustovalov <ipustovalov@swordfishsecurity.com>
2023-01-24 21:00:18 +0300

    tests get unfamiliar user keys for Ex16 add

--- lib/assertions.py
+++ lib/assertions.py
@@ -10,8 +10,8 @@ class Assertions:
         except json.JSONDecodeError:
             assert False, f"Response is not in JSON format. Response text is '{response.text}'"
 
-            assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
-            assert response_as_dict[name] == expected_value, error_message
+        assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
+        assert response_as_dict[name] == expected_value, error_message
 
     @staticmethod
     def assert_json_has_key(response: Response, name):
@@ -20,7 +20,37 @@ class Assertions:
         except json.JSONDecodeError:
             assert False, f"Response is not in JSON format. Response text is '{response.text}'"
 
+        assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
+
+    @staticmethod
+    def assert_json_has_keys(response: Response, names: list):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not in JSON format. Response text is '{response.text}'"
+
+        for name in names:
             assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
+
+    @staticmethod
+    def assert_json_has_no_key(response: Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not in JSON format. Response text is '{response.text}'"
+
+        assert name not in response_as_dict, f"Response JSON shouldn't have key '{name}'. But it's present"
+
+    @staticmethod
+    def assert_json_has_unfamiliar_user_key(response: Response, expected_name, unexpected_names):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not in JSON format. Response text is '{response.text}'"
+
+        assert expected_name in response_as_dict, f"Response JSON doesn't have key '{expected_name}'"
+        for name in unexpected_names:
+            assert name not in response_as_dict, f"Response JSON shouldn't have key '{name}'. But it's present"
 
     @staticmethod
     def asset_code_status(response: Response, expected_status_code):
